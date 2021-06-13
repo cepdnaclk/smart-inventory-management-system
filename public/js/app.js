fedda976f74dbf6f3a0b60e3a72c337accf88aab
Thilin98 <thlini1998@gmail.com>
2021-06-13 21:29:27 +0530

    First commit after installing

--- public/js/app.js
+++ public/js/app.js
@@ -49746,8 +49746,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\wamp64\www\example\smart-inventory-management-system\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\wamp64\www\example\smart-inventory-management-system\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\ProjectCE\smart-inventory-management-system\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\ProjectCE\smart-inventory-management-system\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
