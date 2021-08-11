de2c819acb35ee8b81c288311f2c3ef0a5253c2b
DrMadhushan <drmadhushan@gmail.com>
2021-08-12 01:29:02 +0530

    Added component item table migration, seeder and controller

---
+++ database/migrations/2021_07_28_190230_create_component_items_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComponentItemsTable extends Migration
+{
+    /**
+    * Run the migrations.
+    *
+    * @return void
+    */
+    public function up()
+    {
+        Schema::create('component_items', function (Blueprint $table) {
+            $table->id()->startingValue(1000);;
+            $table->char("code", 8)->default('');
+
+            $table->string("name");
+            $table->string("brand")->nullable();
+            $table->string("productCode")->nullable();
+
+            $table->text("specifications")->nullable();
+            $table->text("description")->nullable();
+
+            $table->boolean("isAvailable")->default(false);
+            $table->float("price")->nullable(); // in LKR
+
+            // Physical size in terms of appearance [small, medium, large] kind of
+            $table->string("size")->nullable()->default('');
+
+            // Thought to have type and family as a property rather than having different tables
+            $table->string("type")->nullable()->default('');
+            $table->string("family")->nullable()->default('');
+
+            $table->string('thumb')->nullable();
+            $table->timestamps();
+
+            /*$table->foreignId('component_type_id')
+                ->constrained()
+                ->references('id')
+                ->onDelete('cascade')
+                ->on('component_types');
+                */
+        });
+    }
+
+    /**
+    * Reverse the migrations.
+    *
+    * @return void
+    */
+    public function down()
+    {
+        Schema::dropIfExists('component_items');
+    }
+}
