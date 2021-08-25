1fd68aa7590fa6d11cb196ab3bdc182b1d6c3734
NuwanJ <nuwanjaliyagoda@gmail.com>
2021-08-25 09:21:10 +0530

    Mobile layout fixes

--- resources/views/frontend/equipment/category.blade.php
+++ resources/views/frontend/equipment/category.blade.php
@@ -6,13 +6,13 @@
     <div class="container py-4">
         <div class="row justify-content-center">
             <div class="col-md-12">
-                <h3>Category: {{ $equipmentType->title }}</h3>
+                <h3>{{ $equipmentType->title }}</h3>
 
                 @if($equipmentType->children()->count() != 0)
                     <div class="container pt-2">Sub-Categories</div>
                     <div class="container pt-2">
                         @foreach($equipmentType->children() as $category)
-                            <a class="btn btn-secondary byn-200 mx-1"
+                            <a class="btn btn-secondary byn-200 mx-1 mb-2"
                                href="{{ route('frontend.equipment.category', $category) }}">{{ $category->title }}</a>
                         @endforeach
                         <hr/>
@@ -25,8 +25,8 @@
                     <div class="container pt-2">
                         <div class="row equal">
                             @foreach($items as $item)
-                                <div class="col-2 p-1 d-flex">
-                                    <div class="m-1 text-center card">
+                                <div class="col-6 col-sm-3 col-md-2 p-1 d-flex">
+                                    <div class="text-center card">
                                         <a class="text-decoration-none"
                                            href="{{ route('frontend.equipment.item', $item) }}">
                                             <img class="img-fluid p-2 mx-auto" src="{{ $item->thumbURL() }}"
