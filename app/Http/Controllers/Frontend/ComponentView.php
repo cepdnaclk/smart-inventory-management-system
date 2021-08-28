78925d61eb8d307c00add8aec9c4dec9bca5c1c2
thanujan96 <thanujan96@gmail.com>
2021-08-28 21:55:09 +0530

    feat: implement frontend route and controller for componentTyoe and items

---
+++ app/Http/Controllers/Frontend/ComponentView.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\ComponentItem;
+use App\Models\ComponentType;
+
+class ComponentView extends Controller
+{
+
+    // Home Page
+    public function index()
+    {
+        $eqTypes = ComponentType::all();
+        return view('frontend.component.index', compact('eqTypes'));
+    }
+
+    // component Category Page
+    public function viewCategory(ComponentType $componentType)
+    {
+        $items = $componentType->hasMany(ComponentItem::class)->paginate(12);;
+        // ->paginate(16);
+        return view('frontend.component.category', compact('items', 'componentType'));
+    }
+
+    // component Item Page
+    public function viewItem(ComponentItem $componentItem)
+    {
+        return view('frontend.component.item', compact('componentItem'));
+    }
+}
