e30a38436a4d95b8371a886a285502a0bf8cb0e2
Thilin98 <thlini1998@gmail.com>
2021-10-22 12:42:05 +0530

    Add the cart controller

---
+++ app/Http/Controllers/Frontend/User/CartController.php
@@ -0,0 +1,22 @@
+<?php  
+
+namespace App\Http\Controllers\Frontend\User;
+
+use Illuminate\Http\Request;
+use App\Models\ComponentItem;
+
+/**
+ * Class CartController.
+ */
+class CartController 
+{
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+
+    public function index()
+    {}    
+
+}
