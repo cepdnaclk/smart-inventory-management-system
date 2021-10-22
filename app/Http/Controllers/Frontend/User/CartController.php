39ab245d66da95c486a742aa77c2cec75533d9f3
Thilin98 <thlini1998@gmail.com>
2021-10-22 20:26:34 +0530

    Implemented remove items and update cart method in comtroller

--- app/Http/Controllers/Frontend/User/CartController.php
+++ app/Http/Controllers/Frontend/User/CartController.php
@@ -49,4 +49,29 @@ class CartController
         return redirect()->back()->with('success', 'Product added to cart successfully!');
     }
 
+    public function update(Request $request)
+    {
+        if($request->id && $request->quantity)
+        {
+            $cart = session()->get('cart');
+            $cart[$request->id]["quantity"] = $request->quantity;
+            session()->put('cart', $cart);
+            session()->flash('success', 'Cart updated successfully');
+        }
+    }
+
+    public function remove(Request $request)
+    {
+        if($request->id)
+        {
+            $cart = session()->get('cart');
+            if(isset($cart[$request->id]))
+            {
+                unset($cart[$request->id]);
+                session()->put('cart', $cart);
+            }
+            session()->flash('success', 'Product removed successfully');
+        }   
+    }
+
 }
