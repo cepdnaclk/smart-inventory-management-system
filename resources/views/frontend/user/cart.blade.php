32464256346f7a02cb6268544be12843e745c78b
Thilin98 <thlini1998@gmail.com>
2021-10-22 20:30:14 +0530

    Add remove item and update cart methods

--- resources/views/frontend/user/cart.blade.php
+++ resources/views/frontend/user/cart.blade.php
@@ -51,6 +51,44 @@
 
 @endsection
 
+
+<script type="text/javascript">
+    $(".update-cart").change(function (e) {
+        e.preventDefault();
+        var ele = $(this);
+        $.ajax({
+            url: '{{ route('frontend.user.update.cart') }}',
+            method: "patch",
+            data: {
+                _token: '{{ csrf_token() }}', 
+                id: ele.parents("tr").attr("data-id"), 
+                quantity: ele.parents("tr").find(".quantity").val()
+            },
+            success: function (response) {
+               window.location.reload();
+            }
+        });
+    });
+
+    $(".remove-from-cart").click(function (e) {
+        e.preventDefault();
+        var ele = $(this);
+        if(confirm("Are you sure want to remove?")) {
+            $.ajax({
+                url: '{{ route('frontend.user.remove.from.cart') }}',
+                method: "DELETE",
+                data: {
+                    _token: '{{ csrf_token() }}', 
+                    id: ele.parents("tr").attr("data-id")
+                },
+                success: function (response) {
+                    window.location.reload();
+                }
+            });
+        }
+    });
+    
+</script>
 @section('scripts')
 
 @endsection
