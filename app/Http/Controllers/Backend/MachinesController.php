1a663d1f552de5692a99acb69e0a814c06055534
NuwanJ <nuwanjaliyagoda@gmail.com>
2021-11-30 15:19:21 +0530

    Implemented the Database model + Controllers for Machines and Raw Materials

--- app/Http/Controllers/Backend/MachinesController.php
+++ app/Http/Controllers/Backend/MachinesController.php
@@ -5,60 +5,81 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Models\Machines;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Intervention\Image\Facades\Image;
 
 class MachinesController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function index()
     {
-        return view('backend.machines.index');
+        $machines = Machines::all()->paginate(16);
+        return view('backend.machines.index', compact('machines'));
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function create()
     {
-        //
+         return view('backend.machines.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse|void
      */
     public function store(Request $request)
     {
-        //
+        $data = request()->validate([
+            'code' => 'string|nullable|max:8',
+            'title' => 'string|required',
+
+        ]);
+
+        try {
+            if ($request->thumb != null) {
+                $data['thumb'] = $this->uploadThumb(null, $request->thumb, "machines");
+            }
+
+            $machine = new Machines($data);
+
+            $machine->save();
+            return redirect()->route('admin.machines.index')->with('Success', 'Machine was created !');
+
+        } catch (\Exception $ex) {
+            return abort(500);
+        }
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\Machines  $machines
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function show(Machines $machines)
     {
-        //
+        return view('backend.machines.show', compact('machines'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  \App\Models\Machines  $machines
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
      */
     public function edit(Machines $machines)
     {
-        //
+        return view('backend.machines.edit', compact('machines'));
     }
 
     /**
@@ -66,21 +87,78 @@ class MachinesController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Machines  $machines
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|void
      */
     public function update(Request $request, Machines $machines)
     {
-        //
+        $data = request()->validate([
+            'code' => 'string|nullable|max:8',
+            'title' => 'string|required',
+
+        ]);
+
+        try {
+            if ($request->thumb != null) {
+                $data['thumb'] = $this->uploadThumb($machines->thumbURL(), $request->thumb, "machines");
+            }
+            $machines->update($data);
+            return redirect()->route('admin.machines.index')->with('Success', 'Machine was updated !');
+
+        } catch (\Exception $ex) {
+            return abort(500);
+        }
+    }
+
+    /**
+     * Confirm to delete the specified resource from storage.
+     * @param \App\Models\Machines $machines
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function delete(Machines $machines)
+    {
+        return view('backend.machines.delete', compact('rawMaterials'));
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Machines  $machines
-     * @return \Illuminate\Http\Response
+     * @param \App\Models\Machines $machines
+     * @return \Illuminate\Http\RedirectResponse|void
      */
     public function destroy(Machines $machines)
     {
-        //
+        try {
+            // Delete the thumbnail form the file system
+            $this->deleteThumb($machines->thumbURL());
+
+            $machines->delete();
+            return redirect()->route('admin.machines.index')->with('Success', 'Machine was deleted !');
+
+        } catch (\Exception $ex) {
+            return abort(500);
+        }
+    }
+
+    private function deleteThumb($currentURL)
+    {
+        if ($currentURL != null) {
+            $oldImage = public_path($currentURL);
+            if (File::exists($oldImage)) unlink($oldImage);
+        }
+    }
+
+    // Private function to handle thumb images
+    private function uploadThumb($currentURL, $newImage, $folder)
+    {
+        // Delete the existing image
+        $this->deleteThumb($currentURL);
+
+        $imageName = time() . '.' . $newImage->extension();
+        $newImage->move(public_path('img/' . $folder), $imageName);
+        $imagePath = "/img/$folder/" . $imageName;
+        $image = Image::make(public_path($imagePath))->fit(360, 360);
+        $image->save();
+
+        return $imageName;
     }
 }
--- app/Http/Controllers/Backend/RawMaterialsController.php
+++ app/Http/Controllers/Backend/RawMaterialsController.php
@@ -5,17 +5,20 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Models\RawMaterials;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Intervention\Image\Facades\Image;
 
 class RawMaterialsController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function index()
     {
-        return view('backend.raw_materials.index');
+        $raw_materials = RawMaterials::all()->paginate(16);
+        return view('backend.raw_materials.index', compact('raw_materials'));
     }
 
     /**
@@ -25,62 +28,153 @@ class RawMaterialsController extends Controller
      */
     public function create()
     {
-        //
+        // return view('backend.raw_materials.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @return void
      */
     public function store(Request $request)
     {
-        //
+        $data = request()->validate([
+            'code' => 'string|nullable|max:8',
+            'title' => 'string|required',
+            'color' => 'string|required',
+            'description' => 'string|nullable',
+            'specifications' => 'string|nullable',
+            'quantity' => 'numeric|nullable',
+            'unit' => 'string|nullable',
+            'thumb' => 'image|nullable|mimes:jpeg,jpg,png,jpg,gif,svg|max:2048',
+//          'enum' => 'string'
+            'notes' => 'string|nullable',
+        ]);
+
+        try {
+            if ($request->thumb != null) {
+                $data['thumb'] = $this->uploadThumb(null, $request->thumb, "raw_materials");
+            }
+
+            $material = new RawMaterials($data);
+
+            $material->save();
+            return redirect()->route('admin.raw_materials.index')->with('Success', 'Raw Material was created !');
+
+        } catch (\Exception $ex) {
+            return abort(500);
+        }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\RawMaterials  $rawMaterials
-     * @return \Illuminate\Http\Response
+     * @param \App\Models\RawMaterials $rawMaterials
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function show(RawMaterials $rawMaterials)
     {
-        //
+        return view('backend.raw_materials.show', compact('rawMaterials'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\RawMaterials  $rawMaterials
-     * @return \Illuminate\Http\Response
+     * @param \App\Models\RawMaterials $rawMaterials
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function edit(RawMaterials $rawMaterials)
     {
-        //
+        return view('backend.raw_materials.edit', compact('rawMaterials'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\RawMaterials  $rawMaterials
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\RawMaterials $rawMaterials
+     * @return \Illuminate\Http\RedirectResponse|void
      */
     public function update(Request $request, RawMaterials $rawMaterials)
     {
-        //
+        $data = request()->validate([
+            'code' => 'string|nullable|max:8',
+            'title' => 'string|required',
+            'color' => 'string|required',
+            'description' => 'string|nullable',
+            'specifications' => 'string|nullable',
+            'quantity' => 'numeric|nullable',
+            'unit' => 'string|nullable',
+            'thumb' => 'image|nullable|mimes:jpeg,jpg,png,jpg,gif,svg|max:2048',
+//          'enum' => 'string'
+            'notes' => 'string|nullable',
+        ]);
+
+        try {
+            if ($request->thumb != null) {
+                $data['thumb'] = $this->uploadThumb($rawMaterials->thumbURL(), $request->thumb, "raw_materials");
+            }
+
+            $rawMaterials->update($data);
+            return redirect()->route('admin.raw_materials.index')->with('Success', 'Raw Material was updated !');
+
+        } catch (\Exception $ex) {
+            return abort(500);
+        }
+    }
+
+    /**
+     * Confirm to delete the specified resource from storage.
+     * @param \App\Models\RawMaterials $rawMaterials
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function delete(RawMaterials $rawMaterials)
+    {
+        return view('backend.raw_materials.delete', compact('rawMaterials'));
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\RawMaterials  $rawMaterials
-     * @return \Illuminate\Http\Response
+     * @param \App\Models\RawMaterials $rawMaterials
+     * @return \Illuminate\Http\RedirectResponse|void
      */
     public function destroy(RawMaterials $rawMaterials)
     {
-        //
+        try {
+            // Delete the thumbnail form the file system
+            $this->deleteThumb($rawMaterials->thumbURL());
+
+            $rawMaterials->delete();
+            return redirect()->route('admin.raw_materials.index')->with('Success', 'Raw material was deleted !');
+
+        } catch (\Exception $ex) {
+            return abort(500);
+        }
+    }
+
+    private function deleteThumb($currentURL)
+    {
+        if ($currentURL != null) {
+            $oldImage = public_path($currentURL);
+            if (File::exists($oldImage)) unlink($oldImage);
+        }
+    }
+
+    // Private function to handle thumb images
+    private function uploadThumb($currentURL, $newImage, $folder)
+    {
+
+        // Delete the existing image
+        $this->deleteThumb($currentURL);
+
+        $imageName = time() . '.' . $newImage->extension();
+        $newImage->move(public_path('img/' . $folder), $imageName);
+        $imagePath = "/img/$folder/" . $imageName;
+        $image = Image::make(public_path($imagePath))->fit(360, 360);
+        $image->save();
+
+        return $imageName;
     }
 }
