71ea9def9cbdddc4df07f7e5ab7a22c51bf3be18
NuwanJ <nuwanjaliyagoda@gmail.com>
2021-11-30 15:19:21 +0530

    Implemented the Database model + Controllers for Machines and Raw Materials

--- app/Models/RawMaterials.php
+++ app/Models/RawMaterials.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class RawMaterials extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+
+    // Return the relative URL of the thumbnail
+    public function thumbURL()
+    {
+        if ($this->thumb != null) return '/img/raw_materials/' . $this->thumb;
+        return null;
+    }
 }
