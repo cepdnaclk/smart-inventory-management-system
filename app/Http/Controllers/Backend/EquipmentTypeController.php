d00630aff2835639ce2be3362210110762e54509
NuwanJ <nuwanjaliyagoda@gmail.com>
2022-05-13 20:11:53 +0530

    Inventory code change and pagination changes

--- app/Http/Controllers/Backend/EquipmentTypeController.php
+++ app/Http/Controllers/Backend/EquipmentTypeController.php
@@ -17,7 +17,7 @@ class EquipmentTypeController extends Controller
      */
     public function index()
     {
-        $equipmentTypes = EquipmentType::paginate(12);
+        $equipmentTypes = EquipmentType::paginate(16);
         return view('backend.equipment.types.index', compact('equipmentTypes'));
     }
 
