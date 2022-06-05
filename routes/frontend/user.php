e64212fc5fc43029a04e1e400f4cd9e31785ef94
rasathuraikaran <e18168@eng.pdn.ac.lk>
2022-06-05 19:49:48 +0530

    -made ordered list

--- routes/frontend/user.php
+++ routes/frontend/user.php
@@ -37,7 +37,7 @@ Route::group(['as' => 'user.', 'middleware' => ['auth', 'password.expires', conf
     Route::delete('remove-from-cart', [CartController::class, 'remove'])->name('remove.from.cart');  
     Route::patch('update-cart', [CartController::class, 'update'])->name('update.cart');
     Route::post('place-order', [CartController::class, 'placeOrder'])->name('place.order');
-    Route::get('my-order',[UserController::class,'index'])->name('placbe.order');
+  
     Route::get('show-my-order',[CartController::class,'showMyOrders'])->name('show.order')  ->breadcrumbs(function (Trail $trail) {
         $trail->parent('frontend.index')
             ->push(__('My Order'), route('frontend.user.account'));
