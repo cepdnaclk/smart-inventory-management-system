890ff222f46e223dbf769f3d12d2d203eeb6c3a2
IshanFernando <e18098@eng.pdn.ac.lk>
2022-06-06 22:49:45 +0530

    add consumable to admin sidebar

--- resources/views/backend/includes/sidebar.blade.php
+++ resources/views/backend/includes/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="c-sidebar c-sidebar-dark c-sidebar-fixed c-sidebar-lg-show" id="sidebar">
     <div class="c-sidebar-brand d-lg-down-none">
-    <!-- <svg class="c-sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
+        <!-- <svg class="c-sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
         <use xlink:href="{{ asset('img/brand/coreui.svg#full') }}"></use>
     </svg>
     <svg class="c-sidebar-brand-minimized" width="46" height="46" alt="CoreUI Logo">
@@ -112,6 +112,29 @@
                 <li class="c-sidebar-nav-item">
                     <x-utils.link
                             :href="route('admin.component.types.index')"
+                            class="c-sidebar-nav-link"
+                            :text="__('Types')"></x-utils.link>
+                </li>
+            </ul>
+        </li>
+
+        <li class="c-sidebar-nav-dropdown">
+            <x-utils.link
+                    href="#"
+                    icon="c-sidebar-nav-icon cil-list"
+                    class="c-sidebar-nav-dropdown-toggle"
+                    :text="__('Consumables')"></x-utils.link>
+
+            <ul class="c-sidebar-nav-dropdown-items">
+                <li class="c-sidebar-nav-item">
+                    <x-utils.link
+                            :href="route('admin.component.items.index')"
+                            class="c-sidebar-nav-link"
+                            :text="__('Items')"></x-utils.link>
+                </li>
+                <li class="c-sidebar-nav-item">
+                    <x-utils.link
+                            :href="route('admin.consumable.types.index')"
                             class="c-sidebar-nav-link"
                             :text="__('Types')"></x-utils.link>
                 </li>
