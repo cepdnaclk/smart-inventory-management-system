ee97378977894cb2aef24c51047f3195edcde680
IshanFernando <e18098@eng.pdn.ac.lk>
2022-06-07 14:48:07 +0530

    consumable routes

--- routes/backend/consumables.php
+++ routes/backend/consumables.php
@@ -1,7 +1,82 @@
 <?php
 
+use App\Http\Controllers\Backend\ConsumableItemController;
 use App\Http\Controllers\Backend\ConsumableTypeController;
 use Tabuna\Breadcrumbs\Trail;
+
+
+Route::get('/consumables', function () {
+    return view('backend.consumable.index');
+})->name('consumable.index')
+    ->breadcrumbs(function (Trail $trail) {
+        $trail->push(__('Home'), route('admin.dashboard'))
+            ->push(__('Consumables'), route('admin.consumable.index'));
+    });
+
+
+//consumable Items --------------------------------------------------------------------
+
+//Index
+Route::get('consumables/items', [ConsumableItemController::class, 'index'])
+    ->name('consumable.items.index')
+    ->breadcrumbs(function (Trail $trail) {
+        $trail->push(__('Home'), route('admin.dashboard'))
+            ->push(__('Consumables'), route('admin.consumable.index'))
+            ->push(__('Items'));
+    });
+
+// Create
+Route::get('consumables/items/create', [ConsumableItemController::class, 'create'])
+    ->name('consumable.items.create')
+    ->breadcrumbs(function (Trail $trail) {
+        $trail->push(__('Home'), route('admin.dashboard'))
+            ->push(__('Consumables'), route('admin.consumable.index'))
+            ->push(__('Items'), route('admin.consumable.items.index'))
+            ->push(__('Create'));
+    });
+
+// Store
+Route::post('consumables/items', [ConsumableItemController::class, 'store'])
+    ->name('consumable.items.store');
+
+// Show
+Route::get('consumables/items/{consumableItem}', [ConsumableItemController::class, 'show'])
+    ->name('consumable.items.show')
+    ->breadcrumbs(function (Trail $trail) {
+        $trail->push(__('Home'), route('admin.dashboard'))
+            ->push(__('Consumables'), route('admin.consumable.index'))
+            ->push(__('Items'), route('admin.consumable.items.index'))
+            ->push(__('Show'));
+    });
+
+// Edit
+Route::get('consumables/items/edit/{consumableItem}', [ConsumableItemController::class, 'edit'])
+    ->name('consumable.items.edit')
+    ->breadcrumbs(function (Trail $trail) {
+        $trail->push(__('Home'), route('admin.dashboard'))
+            ->push(__('Consumables'), route('admin.consumable.index'))
+            ->push(__('Items'), route('admin.consumable.items.index'))
+            ->push(__('Edit'));
+    });
+
+
+// Update
+Route::put('consumables/items/{consumableItem}', [ConsumableItemController::class, 'update'])
+    ->name('consumable.items.update');
+
+// Delete
+Route::get('consumables/items/delete/{consumableItem}', [ConsumableItemController::class, 'delete'])
+    ->name('consumable.items.delete')
+    ->breadcrumbs(function (Trail $trail) {
+        $trail->push(__('Home'), route('admin.dashboard'))
+            ->push(__('Consumables'), route('admin.consumable.index'))
+            ->push(__('Items'), route('admin.consumable.items.index'))
+            ->push(__('Delete'));
+    });
+
+// Destroy
+Route::delete('consumables/items/{consumableItem}', [ConsumableItemController::class, 'destroy'])
+    ->name('consumable.items.destroy');
 
 // Consumable Types -------------------------------------------------------------
 
