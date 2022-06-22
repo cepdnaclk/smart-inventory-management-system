0f23315e8c01f6acb499d1be338c0ab450c00220
ridmajayasundara <e18155@eng.pdn.ac.lk>
2022-06-22 21:01:03 +0530

    created locations table and item_locations table

---
+++ database/seeders/ItemLocationsSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ItemLocationsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    protected $data = [
+
+        array('id'=>'0','location_id'=>0,'item_id'=>'EQ/19/1023'),
+        array('id' => '1',  'location_id' => 1 , 'item_id' => "EQ/17/1038"),
+
+
+    ];
+    public function run()
+    {
+        foreach ($this->data as $index => $setting) {
+            $result = DB::table('item_locations')->insert($setting);
+
+            if (!$result) {
+                $this->command->info("Insert failed at record $index.");
+                return;
+            }
+        }
+
+        $this->command->info('Inserted ' . count($this->data) . ' records to item_locations table');
+
+        
+    }
+}
---
+++ database/seeders/LocationsSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class LocationsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+
+    protected $data = [
+
+        array('id'=>'0','location'=>'Makerspace Lab','parent_location'=>null,'x'=>null,'y'=>null,'z'=>null),
+        array('id' => '1', 'location'=> 'Soldering Station Desk', 'parent_location' => 0,'x' => null, 'y' => null, 'z'=> null),
+        array('id' => 2, 'location'=> 'Item Drawer', 'parent_location' => 1,'x' => null, 'y' => null, 'z'=> null),
+
+
+    ];
+
+    public function run()
+    {
+        foreach($this->data as $index => $setting){
+            $result = DB::table('locations')->insert($setting);
+
+            if (!$result) {
+                $this->command->info("Insert failed at record $index.");
+                return;
+            }
+        }
+        $this->command->info('Inserted ' . count($this->data) . ' records to locations table');
+        }
+    }
+
