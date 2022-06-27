82a205fc045dd1de37142b5e74b556c1f99a9e72
SaadiaJameel <e18147@eng.pdn.ac.lk>
2022-06-27 14:15:52 +0530

    duplicate files removed

--- routes/frontend/stations.php
+++ routes/frontend/stations.php
@@ -4,18 +4,21 @@ use App\Http\Controllers\AddStationController;
 use App\Http\Controllers\Frontend\StationController;
 
 
-
+Route::prefix('stations')->group(function () {
 
     Route::get('/stations', function(){
         return view('frontend.stations.index');
     });
 
     //list the stations
-    Route::get('/stations', [StationController::class, 'index'])->name('stations');
+    Route::get('/', [StationController::class, 'index'])->name('stations');
 
     // To list tools of a station
-    Route::get('/stations/{station}',[StationController::class, 'viewStation'])->name('stations');
+    Route::get('/{station}',[StationController::class, 'viewStation'])->name('stations');
 
-    Route::resource('/addstation', AddStationController::class);
+    
+
+
+});
 
 
