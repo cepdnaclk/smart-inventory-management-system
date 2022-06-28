db92b8e5455dbc82eafd4b84e5cbe554608a0ade
ridmajayasundara <e18155@eng.pdn.ac.lk>
2022-06-29 00:12:47 +0530

    created routes for search functionality

---
+++ routes/backend/search.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\Backend\SearchController;
+use Tabuna\Breadcrumbs\Trail;
+
+Route::middleware(['editAccess'])->group(function(){
+
+    Route::get('/search', [SearchController::class,'index'])->name('search.index')
+        ->breadcrumbs(function (Trail $trail) {
+            $trail->push(__('Home'), route('admin.dashboard'))
+                ->push(__('Search'), route('admin.search.index'));
+        });
+
+        Route::post('/search/results', [SearchController::class,'results'])->name('search.results')
+        ->breadcrumbs(function (Trail $trail) {
+            $trail->push(__('Home'), route('admin.dashboard'))
+                ->push(__('Search'), route('admin.search.index'))
+                ->push(__('Results'), route('admin.search.results'));
+        });
+
+});
