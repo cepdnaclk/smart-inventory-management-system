3c963057534b183b53e894f472058fff407e86ea
SakuniJayasinghe <s.nimnadi@gmai.com>
2022-06-29 15:02:05 +0530

    creatred routes for station backend

--- routes/backend/stations.php
+++ routes/backend/stations.php
@@ -1,144 +1,73 @@
 <?php
 
-use App\Http\Controllers\Backend\EquipmentItemController;
-use App\Http\Controllers\Backend\EquipmentTypeController;
+use App\Http\Controllers\Backend\AdminStationController;
 use Tabuna\Breadcrumbs\Trail;
 
 Route::middleware(['editAccess'])->group(function () {
 
-    Route::get('/equipment', function () {
-        return view('backend.equipment.index');
-    })->name('equipment.index')
+    Route::get('/station', function () {
+        return view('backend.station.index');
+    })->name('station.index')
         ->breadcrumbs(function (Trail $trail) {
             $trail->push(__('Home'), route('admin.dashboard'))
-                ->push(__('Equipment'), route('admin.equipment.index'));
+                ->push(__('Station'), route('admin.station.index'));
         });
 
-    // Equipment Items -------------------------------------------------------------
-
     // Index
-    Route::get('equipment/items', [EquipmentItemController::class, 'index'])
-        ->name('equipment.items.index')
+    Route::get('station', [AdminStationController::class, 'index'])
+        ->name('station.index')
         ->breadcrumbs(function (Trail $trail) {
             $trail->push(__('Home'), route('admin.dashboard'))
-                ->push(__('Equipment'), route('admin.equipment.index'))
-                ->push(__('Items'));
+                ->push(__('Station'), route('admin.station.index'))
+                ;
         });
 
     // Create
-    Route::get('equipment/items/create', [EquipmentItemController::class, 'create'])
-        ->name('equipment.items.create')
+    Route::get('station/create', [AdminStationController::class, 'create'])
+        ->name('station.create')
         ->breadcrumbs(function (Trail $trail) {
             $trail->push(__('Home'), route('admin.dashboard'))
-                ->push(__('Equipment'), route('admin.equipment.index'))
-                ->push(__('Items'), route('admin.equipment.items.index'))
+                ->push(__('Station'), route('admin.station.index'))
                 ->push(__('Create'));
         });
 
     // Store
-    Route::post('equipment/items', [EquipmentItemController::class, 'store'])
-        ->name('equipment.items.store');
+    Route::post('station', [AdminStationController::class, 'store'])
+        ->name('station.store');
 
     // Show
-    Route::get('equipment/items/{equipmentItem}', [EquipmentItemController::class, 'show'])
-        ->name('equipment.items.show')
+    Route::get('station/{station}', [AdminStationController::class, 'show'])
+        ->name('station.show')
         ->breadcrumbs(function (Trail $trail) {
             $trail->push(__('Home'), route('admin.dashboard'))
-                ->push(__('Equipment'), route('admin.equipment.index'))
-                ->push(__('Items'), route('admin.equipment.items.index'))
+                ->push(__('Station'), route('admin.station.index'))
                 ->push(__('Show'));
         });
 
     // Edit
-    Route::get('equipment/items/edit/{equipmentItem}', [EquipmentItemController::class, 'edit'])
-        ->name('equipment.items.edit')
+    Route::get('station/edit/{station}', [AdminStationController::class, 'edit'])
+        ->name('station.edit')
         ->breadcrumbs(function (Trail $trail) {
             $trail->push(__('Home'), route('admin.dashboard'))
-                ->push(__('Equipment'), route('admin.equipment.index'))
-                ->push(__('Items'), route('admin.equipment.items.index'))
+                ->push(__('Station'), route('admin.station.index'))             
                 ->push(__('Edit'));
         });
 
     // Update
-    Route::put('equipment/items/{equipmentItem}', [EquipmentItemController::class, 'update'])
-        ->name('equipment.items.update');
+    Route::put('station/{station}', [AdminStationController::class, 'update'])
+        ->name('station.update');
 
     // Delete
-    Route::get('equipment/items/delete/{equipmentItem}', [EquipmentItemController::class, 'delete'])
-        ->name('equipment.items.delete')
+    Route::get('station/delete/{station}', [AdminStationController::class, 'delete'])
+        ->name('station.delete')
         ->breadcrumbs(function (Trail $trail) {
             $trail->push(__('Home'), route('admin.dashboard'))
-                ->push(__('Equipment'), route('admin.equipment.index'))
-                ->push(__('Items'), route('admin.equipment.items.index'))
+                ->push(__('Station'), route('admin.station.index'))
                 ->push(__('Delete'));
         });
 
     // Destroy
-    Route::delete('equipment/items/{equipmentItem}', [EquipmentItemController::class, 'destroy'])
-        ->name('equipment.items.destroy');
-
-
-    // Equipment Types -------------------------------------------------------------
-
-    // Index
-    Route::get('equipment/types', [EquipmentTypeController::class, 'index'])
-        ->name('equipment.types.index')
-        ->breadcrumbs(function (Trail $trail) {
-            $trail->push(__('Home'), route('admin.dashboard'))
-                ->push(__('Equipment'), route('admin.equipment.index'))
-                ->push(__('Types'));
-        });
-
-    // Create
-    Route::get('equipment/types/create', [EquipmentTypeController::class, 'create'])
-        ->name('equipment.types.create')
-        ->breadcrumbs(function (Trail $trail) {
-            $trail->push(__('Home'), route('admin.dashboard'))
-                ->push(__('Equipment'), route('admin.equipment.index'))
-                ->push(__('Types'), route('admin.equipment.types.index'))
-                ->push(__('Create'));
-        });
-
-    // Store
-    Route::post('equipment/types/', [EquipmentTypeController::class, 'store'])
-        ->name('equipment.types.store');
-
-    // Show
-    Route::get('equipment/types/{equipmentType}', [EquipmentTypeController::class, 'show'])
-        ->name('equipment.types.show')
-        ->breadcrumbs(function (Trail $trail) {
-            $trail->push(__('Home'), route('admin.dashboard'))
-                ->push(__('Equipment'), route('admin.equipment.index'))
-                ->push(__('Types'), route('admin.equipment.types.index'))
-                ->push(__('Show'));
-        });
-
-    // Edit
-    Route::get('equipment/types/edit/{equipmentType}', [EquipmentTypeController::class, 'edit'])
-        ->name('equipment.types.edit')
-        ->breadcrumbs(function (Trail $trail) {
-            $trail->push(__('Home'), route('admin.dashboard'))
-                ->push(__('Equipment'), route('admin.equipment.index'))
-                ->push(__('Types'), route('admin.equipment.types.index'))
-                ->push(__('Edit'));
-        });
-
-    // Update
-    Route::put('equipment/types/{equipmentType}', [EquipmentTypeController::class, 'update'])
-        ->name('equipment.types.update');
-
-    // Delete
-    Route::get('equipment/types/delete/{equipmentType}', [EquipmentTypeController::class, 'delete'])
-        ->name('equipment.types.delete')
-        ->breadcrumbs(function (Trail $trail) {
-            $trail->push(__('Home'), route('admin.dashboard'))
-                ->push(__('Equipment'), route('admin.equipment.index'))
-                ->push(__('Types'), route('admin.equipment.types.index'))
-                ->push(__('Delete'));
-        });
-
-    // Destroy
-    Route::delete('equipment/types/{equipmentType}', [EquipmentTypeController::class, 'destroy'])
-        ->name('equipment.types.destroy');
+    Route::delete('station/{station}', [AdminStationController::class, 'destroy'])
+        ->name('station.destroy');
 
 });
