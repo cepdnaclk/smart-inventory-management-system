7849edb83c5675c8ffc9ddfbeda3bd6bec164c0e
Nuwan Jaliyagoda <NuwanJ@users.noreply.github.com>
2022-07-02 13:59:58 +0530

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,10 +4,7 @@ namespace Database\Seeders;
 
 use Database\Seeders\Traits\TruncateTable;
 use Illuminate\Database\Eloquent\Model;
-
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Eloquent\Model;
-use Database\Seeders\Traits\TruncateTable;
 
 /**
  * Class DatabaseSeeder.
@@ -35,17 +32,11 @@ class DatabaseSeeder extends Seeder
         $this->call(EquipmentItemSeeder::class);
         $this->call(ComponentTypeSeeder::class);
         $this->call(ComponentItemSeeder::class);
-        
         $this->call(ConsumableTypeSeeder::class);
         $this->call(ConsumableItemSeeder::class);
-
         $this->call(RawMaterialsSeeder::class);
         $this->call(MachinesSeeder::class);
         $this->call(JobRequestsSeeder::class);
-
-        $this->call(StationsSeeder::class);
-        $this->call(ReservationsSeeder::class);
-
 
         Model::reguard();
     }
