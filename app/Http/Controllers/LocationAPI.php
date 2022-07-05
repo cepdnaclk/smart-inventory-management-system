73b37ee2f8a0d78a22279e2360a41e7e054b8dcf
IshanFernando <e18098@eng.pdn.ac.lk>
2022-07-05 08:58:04 +0530

    Remove unused imports

--- app/Http/Controllers/LocationAPI.php
+++ app/Http/Controllers/LocationAPI.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\ItemLocations;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\RateLimiter;
 
 class LocationAPI extends Controller
 {
