ce302a0a786bb6a12a02ed5ce864c3de44fb75bd
NipunFernando <e18100@eng.pdn.ac.lk>
2022-07-06 04:00:50 +0530

    all seeders added

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Database\Seeders\Traits\TruncateTable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
+
+/**
+ * Class DatabaseSeeder.
+ */
+class DatabaseSeeder extends Seeder
+{
+    use TruncateTable;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        $this->truncateMultiple([
+            'activity_log',
+            'failed_jobs',
+        ]);
+
+        $this->call(AuthSeeder::class);
+        $this->call(AnnouncementSeeder::class);
+
+        $this->call(EquipmentTypeSeeder::class);
+        $this->call(EquipmentItemSeeder::class);
+        $this->call(ComponentTypeSeeder::class);
+        $this->call(ComponentItemSeeder::class);
+        $this->call(RawMaterialsSeeder::class);
+        $this->call(MachinesSeeder::class);
+        $this->call(JobRequestsSeeder::class);
+
+        Model::reguard();
+    }
+}
