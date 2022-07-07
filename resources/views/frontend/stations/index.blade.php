2326ae2eb81471b3b168b00f0e49c0c93612dc31
Thamish <e18379@eng.pdn.ac.lk>
2022-07-07 19:12:48 +0530

    row-wise listed stations with images

--- resources/views/frontend/stations/index.blade.php
+++ resources/views/frontend/stations/index.blade.php
@@ -12,12 +12,25 @@
                 <div class="container">
                     <ul>
                         @foreach($stations as $station)
-                            <li>
-                                <a href="{{ route('frontend.stations.station',$station->id ) }}">{{ $station->stationName  }}</a>
-                            </li>
+                            <ul>
+                                <div class="col-md-4 col-sm-12 col-12 d-flex mb-4">
+                                    @if( $station->thumb != null )
+                                        <img src="{{ $station->thumbURL() }}"
+                                        alt="{{ $station->stationName }}"
+                                        class="img img-thumbnail img-fluid p-3 mx-auto">
+                                    @else
+                                        {{-- TODO: Add a default image --}}
+                                        <span>[Not Available]</span>
+                                    @endif
+
+                                    <div class="col-md-8 col-sm-12 col-12 mb-4">
+                                        <a href="{{ route('frontend.stations.station',$station->id ) }}">{{ $station->stationName  }}</a>
+                                    </div>
+                                </div>
+                            </ul>
                         @endforeach
                     </ul>
-                </div>
+                </div> 
             </div>
         </div>
     </div>
