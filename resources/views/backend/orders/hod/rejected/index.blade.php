7f9e92f069f6f0b4dcbf10e9f86ad75346cf217c
rasathuraikaran <e18168@eng.pdn.ac.lk>
2022-07-10 19:22:54 +0530

    added the H_O_D dashboard for Order request

---
+++ resources/views/backend/orders/hod/rejected/index.blade.php
@@ -0,0 +1,73 @@
+@extends('backend.layouts.app')
+
+@section('title', __('Order Requests - Lecturer View'))
+
+@section('breadcrumb-links')
+@endsection
+
+@section('content')
+    <div>
+        <x-backend.card>
+            <x-slot name="header">
+            Order Accepted - Lecturer View
+            </x-slot>
+
+            <x-slot name="body">
+                @if (session('Success'))
+                    <div class="alert alert-success">
+                        {{ session('Success') }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                @endif
+
+                <div class="container table-responsive pt-3">
+              
+                    <h4 class="pb-3">REJECTED</h4>
+                    <table class="table table-striped align-middle">
+
+                        <tr>
+                            <th>OrderID</th>
+                            <th>User Name</th>
+                            <th>status</th>
+                            <th>OrderedDate</th>
+                           
+                            <th>View </th>
+                        </tr>
+
+                        @foreach($orderApproval as $order)
+                        @if ($order->orders->status=="REJECTED")
+                                <tr>
+                                   
+                                    <td>{{ $order->orders->id }}</td>
+                                    <td>{{ $order->orders->user->name }}</td>
+                                    <td>
+                                        {{ $order->orders->status }}</td>
+
+                                        <td>
+                                            {{ $order->orders->ordered_date }}</td>
+
+                                        
+                                            <td class="d-flex justify-content-end">
+                                                <div class="btn-group" role="group">
+                                                    <a href="{{ route('admin.orders.h_o_d.show',$order->orders)}}"
+                                                       class="btn btn-primary btn-xs">
+                                                        <i class="fa fa-check" title="Approval"></i>
+                                                    </a>
+                                                  </td>
+
+                                </tr>
+                                @endif
+                        @endforeach
+                    </table>
+
+                </div>
+
+
+               
+          
+            </x-slot>
+        </x-backend.card>
+    </div>
+@endsection
