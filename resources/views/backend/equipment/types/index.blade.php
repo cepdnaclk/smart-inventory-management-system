8c26aa0c6caea04e36086dbfe5e82a9988f33efd
IshanFernando <e18098@eng.pdn.ac.lk>
2022-07-11 22:15:26 +0530

    Change EquipmentType tables

--- resources/views/backend/equipment/types/index.blade.php
+++ resources/views/backend/equipment/types/index.blade.php
@@ -34,54 +34,56 @@
                     </div>
                 @endif
 
-                <div class="container table-responsive pt-3">
-                    <table class="table table-striped">
-                        <tr>
-                            <th>Code</th>
-                            <th>Title</th>
-                            <th>Parent Category</th>
-                            <th>Description</th>
-                            <th>&nbsp;</th>
-                        </tr>
+                    <livewire:backend.equipment-type-table/>
 
-                        @foreach($equipmentTypes as $equipmentType)
-                            <tr>
-                                <td>{{ $equipmentType->inventoryCode()  }}</td>
-                                <td>{{ $equipmentType->title  }}</td>
-                                <td>
-                                    @if( $equipmentType->parent() !== null)
-                                        <a href="{{ route('admin.equipment.types.show', $equipmentType->parent()->id) }}">
-                                            {{ $equipmentType->parent()->title }}
-                                        </a>
-                                    @else
-                                        N/A
-                                    @endif
-                                </td>
-                                {{--                                <td>{{ $equipmentType->subtitle ?? 'N/A' }}</td>--}}
-                                <td>{{ $equipmentType->description  }}</td>
-                                <td>
-                                    <div class="d-flex px-0 mt-0 mb-0">
-                                        <div class="btn-group" role="group" aria-label="Basic example">
-                                            <a href="{{ route('admin.equipment.types.show', $equipmentType)}}"
-                                               class="btn btn-secondary btn-xs"><i class="fa fa-eye" title="Show"></i>
-                                            </a>
+{{--                    <div class="container table-responsive pt-3">--}}
+{{--                    <table class="table table-striped">--}}
+{{--                        <tr>--}}
+{{--                            <th>Code</th>--}}
+{{--                            <th>Title</th>--}}
+{{--                            <th>Parent Category</th>--}}
+{{--                            <th>Description</th>--}}
+{{--                            <th>&nbsp;</th>--}}
+{{--                        </tr>--}}
 
-                                            <a href="{{ route('admin.equipment.types.edit', $equipmentType)}}"
-                                               class="btn btn-info btn-xs"><i class="fa fa-pencil" title="Edit"></i>
-                                            </a>
-                                            <a href="{{ route('admin.equipment.types.delete', $equipmentType)}}"
-                                               class="btn btn-danger btn-xs"><i class="fa fa-trash"
-                                                                                title="Delete"></i>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </td>
+{{--                        @foreach($equipmentTypes as $equipmentType)--}}
+{{--                            <tr>--}}
+{{--                                <td>{{ $equipmentType->inventoryCode()  }}</td>--}}
+{{--                                <td>{{ $equipmentType->title  }}</td>--}}
+{{--                                <td>--}}
+{{--                                    @if( $equipmentType->parent() !== null)--}}
+{{--                                        <a href="{{ route('admin.equipment.types.show', $equipmentType->parent()->id) }}">--}}
+{{--                                            {{ $equipmentType->parent()->title }}--}}
+{{--                                        </a>--}}
+{{--                                    @else--}}
+{{--                                        N/A--}}
+{{--                                    @endif--}}
+{{--                                </td>--}}
+{{--                                --}}{{--                                <td>{{ $equipmentType->subtitle ?? 'N/A' }}</td>--}}
+{{--                                <td>{{ $equipmentType->description  }}</td>--}}
+{{--                                <td>--}}
+{{--                                    <div class="d-flex px-0 mt-0 mb-0">--}}
+{{--                                        <div class="btn-group" role="group" aria-label="Basic example">--}}
+{{--                                            <a href="{{ route('admin.equipment.types.show', $equipmentType)}}"--}}
+{{--                                               class="btn btn-secondary btn-xs"><i class="fa fa-eye" title="Show"></i>--}}
+{{--                                            </a>--}}
 
-                            </tr>
-                        @endforeach
-                    </table>
-                    {{ $equipmentTypes->links() }}
-                </div>
+{{--                                            <a href="{{ route('admin.equipment.types.edit', $equipmentType)}}"--}}
+{{--                                               class="btn btn-info btn-xs"><i class="fa fa-pencil" title="Edit"></i>--}}
+{{--                                            </a>--}}
+{{--                                            <a href="{{ route('admin.equipment.types.delete', $equipmentType)}}"--}}
+{{--                                               class="btn btn-danger btn-xs"><i class="fa fa-trash"--}}
+{{--                                                                                title="Delete"></i>--}}
+{{--                                            </a>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
+{{--                                </td>--}}
+
+{{--                            </tr>--}}
+{{--                        @endforeach--}}
+{{--                    </table>--}}
+{{--                    {{ $equipmentTypes->links() }}--}}
+{{--                </div>--}}
             </x-slot>
         </x-backend.card>
     </div>
