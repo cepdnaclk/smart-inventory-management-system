c25a1838c1fc57e567c1f8ba87ed776147d30421
SaadiaJameel <e18147@eng.pdn.ac.lk>
2022-07-11 22:48:34 +0530

    routes to stations

--- resources/views/frontend/reservation/delete.blade.php
+++ resources/views/frontend/reservation/delete.blade.php
@@ -2,9 +2,7 @@
 
 @section('title', __('Reservation')) 
 
-@section('breadcrumb-links')
-    @include('backend.reservation.includes.breadcrumb-links')
-@endsection 
+
 
 @section('content')
     <div>
--- resources/views/frontend/reservation/show.blade.php
+++ resources/views/frontend/reservation/show.blade.php
@@ -2,9 +2,6 @@
 
 @section('title', __('Reservation'))
 
-@section('breadcrumb-links')
-    @include('backend.reservation.includes.breadcrumb-links')
-@endsection 
 
 @section('content')
     <div>
