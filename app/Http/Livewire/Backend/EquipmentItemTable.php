c83857bb674b6ca3567649f7f4281946c597a50a
IshanFernando <e18098@eng.pdn.ac.lk>
2022-07-11 21:27:17 +0530

    All filtering to EquipmentItem Livewire tables

--- app/Http/Livewire/Backend/EquipmentItemTable.php
+++ app/Http/Livewire/Backend/EquipmentItemTable.php
@@ -3,10 +3,13 @@
 namespace App\Http\Livewire\Backend;
 
 use App\Models\EquipmentItem;
+use App\Models\EquipmentType;
 use Illuminate\Database\Eloquent\Builder;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 use App\Models\ConsumableItem;
+use Rappasoft\LaravelLivewireTables\Views\Filter;
+use function Livewire\str;
 
 class EquipmentItemTable extends DataTableComponent
 {
@@ -22,7 +25,8 @@ class EquipmentItemTable extends DataTableComponent
             Column::make("Product Code and Brand"),
             Column::make("Quantity", 'quantity')
                 ->sortable(),
-            Column::make("Category"),
+            Column::make("Category", 'equipment_type_id')
+                ->sortable(),
             Column::make("Price (LKR)", 'price')
                 ->sortable(),
             Column::make("Dimensions(cm) WxLxH"),
@@ -34,7 +38,27 @@ class EquipmentItemTable extends DataTableComponent
 
     public function query(): Builder
     {
-        return EquipmentItem::query();
+        return EquipmentItem::query()
+            ->when($this->getFilter('category'), fn($query, $category) => $query->where('equipment_type_id', $category));
+    }
+
+    public function filters(): array
+    {
+        // Category ------------------------------------------------------
+        $categoriesFromDB = EquipmentType::pluck('title','id')->toArray();
+
+        // Add '' => "Any" to the beginning of the array
+        $categories = array();
+        $categories[''] = "Any";
+        foreach ($categoriesFromDB as $key => $value) {
+            $categories[$key] = $value;
+        }
+
+
+        return [
+            'category' => Filter::make('Category')
+                ->select($categories)
+        ];
     }
 
     public function rowView(): string
