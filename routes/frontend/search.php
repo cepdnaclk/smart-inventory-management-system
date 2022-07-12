240360a50ac328c684aad717fc01f94d7d15e68b
ridmajayasundara <e18155@eng.pdn.ac.lk>
2022-07-12 09:47:40 +0530

    re-created search controller

--- routes/frontend/search.php
+++ routes/frontend/search.php
@@ -5,11 +5,17 @@ use Tabuna\Breadcrumbs\Trail;
 
 
 
-    Route::post('/search/results', [SearchController::class, 'results'])->name('frontSearch.results')
-        ->breadcrumbs(function (Trail $trail) {
-            $trail->push(__('Home'), route('frontend.index'))
-                ->push(__('Results'), route('frontSearch.results'));
-        });
-        //might have to edit the breadcrumbs - routes ^ are wrong ?
+    Route::post('/search/results', [SearchController::class, 'results'])->name('frontSearch.results');
+   // ->breadcrumbs(function (Trail $trail) {
+    //        $trail->push(__('Home'), route('frontend.index'))
+    //            ->push(__('Results'), route('frontSearch.results'));
+     //   }); 
+
+
+        
+//might have to edit the breadcrumbs - routes ^ are wrong ?
+//something is wrong with the breadcrumbs 
+//fix it
+
 
    
