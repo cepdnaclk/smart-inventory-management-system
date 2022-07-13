dd18f0eaaa5dd5d46550384648db17de146aa64a
ridmajayasundara <e18155@eng.pdn.ac.lk>
2022-07-13 19:06:56 +0530

    added breadcrumbs to frontend search

--- routes/frontend/search.php
+++ routes/frontend/search.php
@@ -5,17 +5,10 @@ use Tabuna\Breadcrumbs\Trail;
 
 
 
-    Route::post('/search/results', [SearchController::class, 'results'])->name('frontSearch.results');
-   // ->breadcrumbs(function (Trail $trail) {
-    //        $trail->push(__('Home'), route('frontend.index'))
-    //            ->push(__('Results'), route('frontSearch.results'));
-     //   }); 
-
-
-        
-//might have to edit the breadcrumbs - routes ^ are wrong ?
-//something is wrong with the breadcrumbs 
-//fix it
-
+    Route::post('/search/results', [SearchController::class, 'results'])->name('frontSearch.results')
+        ->breadcrumbs(function (Trail $trail) {
+            $trail->push(__('Home'), route('frontend.index'))
+                ->push(__('Results'), route('frontend.frontSearch.results'));
+        });
 
    
