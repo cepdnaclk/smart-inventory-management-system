b0339c7219911ce563885dc002e2566b742d1463
SakuniJayasinghe <s.nimnadi@gmai.com>
2022-08-02 21:46:57 +0530

    finalize approval views

--- app/Http/Controllers/Backend/ReservationController.php
+++ app/Http/Controllers/Backend/ReservationController.php
@@ -207,110 +207,22 @@ class ReservationController extends Controller
 
     public function update_main(Request $request, Reservation $reservation)
     {
-        $dateOriginal = (new DateTime($reservation->start_date))->format('Y-m-d');
-        $dateOriginal1 = (new DateTime($reservation->start_date))->format('Y-m-d H:i:s');
-
-        $userLoggedin = auth()->user();
-
         $data = request()->validate([
-            'station_id' => 'numeric|required',
-            'start_date' => 'required|date_format:Y-m-d H:i:s',
-            'end_date' => 'required|date_format:Y-m-d H:i:s',
-            'E_numbers' => 'required|regex:^E/\d{2}/\d{3}$^', // TODO: Validate E-Numbers
-            'thumb' => 'image|nullable|mimes:jpeg,jpg,png,jpg,gif,svg|max:4096', // TODO: Maybe we need to increase the file size
-            'thumb_after' => 'image|nullable|mimes:jpeg,jpg,png,jpg,gif,svg|max:4096', // TODO: Maybe we need to increase the file size
             'status' => 'string|nullable',
             'comments' => 'string|nullable'
         ]);
 
-        if ($request->thumb != null) {
-            $data['thumb'] = $this->uploadThumb($reservation->thumbURL(), $request->thumb, "reservations");
-        }
-
-        if ($request->thumb_after != null) {
-            $data['thumb_after'] = $this->uploadThumb($reservation->thumbURL_after(), $request->thumb_after, "reservations_after");
-        }
-
-        // $reservation->update($data);
-        // return redirect()->route('frontend.reservation.index')->with('Success', 'Reservation was updated !');
-
-        $dateNew = (new DateTime($data['start_date']))->format('Y-m-d');
-        $dateNew1 = (new DateTime($data['start_date']))->format('Y-m-d H:i:s');
-
-        $date1 = Carbon::createFromFormat('Y-m-d', $dateOriginal);
-        $date2 = Carbon::createFromFormat('Y-m-d', $dateNew);
-        $result = $date1->eq($date2);
-
-        // dd($result);
-
-        $start = new DateTime($request['start_date']);
-        $end = new DateTime($request['end_date']);
-        $diff = $start->diff($end);
-        $minutes = ($diff->h * 60) + ($diff->s / 60) + ($diff->i) + ($diff->d * 24 * 60) + ($diff->m * 30 * 24 * 60) + ($diff->y * 365 * 24 * 60);
-
-        //For overlap check
-        $res = Reservation::whereDate('start_date', $start)->where('station_id', $data['station_id'])->get();
-
-        $flag = false;
-        foreach ($res as $r) {
-            if ($r->user_id != $userLoggedin['id']) {
-                $flag = $this->isAnOverlapEvent($start, $end, $r);
-            }
-        }
-       
-        // See if the user has already made a reservation on that day for this station
-        $bookings1 = Reservation::whereDate('start_date', $start)->where('user_id', $userLoggedin['id'])->where('station_id', $data['station_id'])->get();
-
-        // See whether the reservation is being made too early
-
-        $todayDate = date('Y-m-d H:i:s');
-        $today = new DateTime($todayDate);
-        $today->add(new DateInterval('PT5H30M'));
-
-        $date3 = Carbon::createFromFormat('Y-m-d H:i:s', $dateOriginal1);
-        $date4 = Carbon::createFromFormat('Y-m-d H:i:s', $dateNew1);
-        $result1 = $date3->eq($date4);
-
-        // dd($start);
-
-        $resDiff = $today->diff($start);
-        $minutesDiff = ($resDiff->h*60) + ($resDiff->s/60) + ($resDiff->i) + ($resDiff->d*24*60) + ($resDiff->m*30*24*60) + ($resDiff->y*365*24*60);
-    
-        
-
         $data = [
-            'station_id' => $request['station_id'],
-            'start_date' => $request['start_date'],
-            'end_date' => $request['end_date'],
-            'E_numbers' => $request['E_numbers'],
-            'duration' => $minutes,
-            'thumb' => $request->thumb,
-            'thumb_after' => $request->thumb_after,
             'status' => $request['status'],
             'comments' => $request['comments'],
             
         ];
-        
 
-        if ($userLoggedin['id'] != $reservation->user_id){
-            return redirect()->route('admin.reservation.index')->with('Error', 'You can not update this reservation');   
-        }elseif(($minutesDiff > 43200)){
-            return redirect()->route('admin.reservation.index')->with('Error', 'You can not make a reservation for that date this early');   
-        }elseif($data['duration'] > 240){
-            return redirect()->route('admin.reservation.index')->with('Error', 'Reservation was not updated! Reservation can not exceed 4 hours');           
-        }elseif($flag){
-            return redirect()->route('admin.reservation.index')->with('Error', 'Reservation was not updated! Time slot not available');   
-        }elseif($today >= $start && !$result1 ){
-            return redirect()->route('admin.reservation.index')->with('Error', 'Reservation was not updated! You can not make a reservation for a date that has passed'); 
-        }elseif((count($bookings1) == 1) && $result && !$flag && ($minutesDiff < 43200)){
             $reservation->update($data);
-            return redirect()->route('admin.reservation.index')->with('Success', 'Reservation was updated !');            
-        }elseif((count($bookings1) == 0 && !$flag && ($minutesDiff < 43200))){
-            $reservation->update($data);
-            return redirect()->route('admin.reservation.index')->with('Success', 'Reservation was updated !');
-        } elseif ((count($bookings1) == 1) && !$result) {
-            return redirect()->route('admin.reservation.index')->with('Error', 'Reservation was not updated! Can not make multiple reservations in one day');
-        }
+            return redirect()->route('admin.reservation.index')->with('Success', 'Reservation status was saved !');            
+     
+
+
     }
 
     public function isAnOverlapEvent(DateTime $eventStartDay, DateTime $eventEndDay, Reservation $res)
