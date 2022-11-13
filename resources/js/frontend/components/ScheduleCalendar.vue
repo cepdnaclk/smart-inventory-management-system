997891bc4ac2d31ee0db497ae1772d16e6f9fd04
Nuwan Jaliyagoda <nuwanjaliyagoda@gmail.com>
2022-11-13 17:03:05 +0530

    Added Vue full calendar

---
+++ resources/js/frontend/components/ScheduleCalendar.vue
@@ -0,0 +1,22 @@
+<script>
+
+import FullCalendar from '@fullcalendar/vue'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction';
+
+export default {
+    components: {
+        FullCalendar // make the <FullCalendar> tag available
+    },
+    data() {
+        return {
+            calendarPlugins: [dayGridPlugin, interactionPlugin]
+        }
+    }
+}
+
+</script>
+
+<template>
+    <FullCalendar defaultView="dayGridMonth" :plugins="calendarPlugins" />
+</template>
