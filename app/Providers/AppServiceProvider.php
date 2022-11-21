4ef6228e29544ebb67808b4dde0bcbb39dee277c
thanujan96 <thanujan96@gmail.com>
2022-11-22 01:17:03 +0530

    bug: in production forcing url to https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Laravel\Sanctum\PersonalAccessToken;
 use Laravel\Sanctum\Sanctum;
 
@@ -34,7 +35,7 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
         Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
         if ($this->app->environment('production')) {
-            \URL::forceSchema('https');
+            URL::forceScheme('https');
         }
     }
 }
