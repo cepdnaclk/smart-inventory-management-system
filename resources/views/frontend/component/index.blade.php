78839233225e8b048ddf8d624aff5e543905f053
Nuwan Jaliyagoda <nuwanjaliyagoda@gmail.com>
2023-05-01 16:36:12 +0530

    Bug fix

--- resources/views/frontend/component/index.blade.php
+++ resources/views/frontend/component/index.blade.php
@@ -39,35 +39,5 @@
                     @endforeach
                 </div>
             </div>
-
-            {{--  --}}
-
-            <div class="row justify-content-center">
-                <div class="col-md-12">
-                    <h3>Browse Components by Category</h3>
-
-                    <div class="container">
-                        <ul>
-                            @foreach ($componentType as $type)
-                                @if ($type->parent() == null)
-                                    <li>
-                                        <a href="{{ route('frontend.component.category', $type) }}">{{ $type->title }}</a>
-                                        @if ($type->children() != null)
-                                            <ul>
-                                                @foreach ($type->children() as $child)
-                                                    <li>
-                                                        <a
-                                                            href="{{ route('frontend.component.category', $child) }}">{{ $child->title }}</a>
-                                                    </li>
-                                                @endforeach
-                                            </ul>
-                                        @endif
-                                    </li>
-                                @endif
-                            @endforeach
-                        </ul>
-                    </div>
-                </div>
-            </div>
         </div>
     @endsection
