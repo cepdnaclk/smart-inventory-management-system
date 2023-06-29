cde93ed05d77499a8439f10f7f11703b255421a8
Nuwan Jaliyagoda <NuwanJ@users.noreply.github.com>
2023-06-29 22:19:15 +0530

    336 limit self registration only for the department students (#338)
    * Implementation
    
    * Added unit testing for DepartmentDataService class

--- config/constants.php
+++ config/constants.php
@@ -3,5 +3,9 @@
 return [
     'frontend' => [
         'dummy_thumb' => '/dummy/item_thumbnail.jpg',
+    ],
+    'department_data' => [
+        'base_url' => 'https://api.ce.pdn.ac.lk',
+        'cache_duration' => 43200 // 6 hours 
     ]
 ];
