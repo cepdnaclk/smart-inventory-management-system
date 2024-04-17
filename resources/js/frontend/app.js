954d2fee0f4faaca3cdeb430496ee5e53479574e
NuwanJ <nuwanjaliyagoda@gmail.com>
2024-04-17 14:00:57 +0530

    Server changes

--- resources/js/frontend/app.js
+++ resources/js/frontend/app.js
@@ -20,10 +20,12 @@ import Vue from 'vue'
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
+/*
 Vue.component(
     'example-component',
     require('./components/ExampleComponent.vue').default
 )
+*/
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
